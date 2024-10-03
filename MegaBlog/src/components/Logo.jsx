5fd0f3a2db97a4135e1d08a320831bfd49d14e65
santanuJana <santanuj2000@gmail.com>
2024-10-03 10:40:06 +0530

    first commit

---
+++ MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Logo({width='10px'}){
+    return(
+        <div >Logo
+            <img src="https://cdn.pixabay.com/photo/2015/03/16/08/18/hex-675576_1280.jpg" alt=""  width="100px" height="100px"/>
+        </div>
+    )
+}
+export default Logo
---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup"
+import Login from "./Login.jsx"
+import PostForm from "./Post-Form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+
+export{
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Button,
+    Input,
+    Select
+}
